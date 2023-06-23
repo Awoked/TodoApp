38534240ce98148f7b743978f86a5a7a870fa593
Alper Koşay <alper.kossay@gmail.com>
2023-06-23 18:45:12 +0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,28 @@
-"use client"
-import TodoList from '@/components/TodoList'
-import useTodo from '@/hooks/useTodo'
 
-export default function Home() {
-    const { todoData } = useTodo();
+import TodoList from '@/components/TodoList'
+import { MongoClient } from 'mongodb'
+
+export default async function Home() {
+
+    const mongoClient = new MongoClient('mongodb+srv://alperksy3434:ftfMdHVLGL9qk9XU@alperdatabase.xr7hjdn.mongodb.net/todo?retryWrites=true&w=majority');
+    const collection = mongoClient.db().collection('todo');
+    const data = await collection.find().toArray();
+    console.log(data);
+
+
 
     return (
         <>
             <section className="py-4">
                 <div className="container">
+                    {
+                        data.length > 0 &&
+                        data.map((data, index) => (
+                            <div key={index}>
+                                {data.name}
+                            </div>
+                        ))
+                    }
                     <div className="todos-wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
                         <TodoList
                             status={"todo"}
--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -3,123 +3,134 @@ import useTodo from '@/hooks/useTodo';
 import React, { useState, useEffect } from 'react'
 
 import { BsPlus, BsCheck } from "react-icons/bs";
-import {IoIosClose} from "react-icons/io";
-import {AiOutlineRight} from "react-icons/ai";
+import { IoIosClose } from "react-icons/io";
+import { AiOutlineRight } from "react-icons/ai";
 
 import { v4 as uuidv4 } from "uuid";
 
-const TodoList = ({ status }) => {
+const TodoList = async ({ status }) => {
 
-  const { data: todoData, add: addTodo, remove: removeTodo } = useTodo();
+    const { data: todoData, add: addTodo, remove: removeTodo } = useTodo();
 
-  const [todoText, setTodoText] = useState("");
+    const [todoText, setTodoText] = useState("");
 
-  const addTodoHandler = (e) => {
-    e.preventDefault();
-    setTodoText("");
-    addTodo([...todoData, {
-      id: uuidv4(),
-      content: todoText,
-      status: "todo"
-    }])
+    const addTodoHandler = (e) => {
+        e.preventDefault();
+        setTodoText("");
+        addTodo([...todoData, {
+            id: uuidv4(),
+            content: todoText,
+            status: "todo"
+        }])
 
-  }
+    }
 
-  const todoStatusHandler = (id, status) => {
-    const updatedData = todoData.map(data => {
-      if (data.id === id) {
-        return {
-          ...data,
-          status: status === "todo" ? "started" : status === "started" ? "done" : data.status
-        };
-      }
-      return data;
-    });
-    addTodo(updatedData);
-  };
+    const todoStatusHandler = (id, status) => {
+        const updatedData = todoData.map(data => {
+            if (data.id === id) {
+                return {
+                    ...data,
+                    status: status === "todo" ? "started" : status === "started" ? "done" : data.status
+                };
+            }
+            return data;
+        });
+        addTodo(updatedData);
+    };
 
-  return (
-    <>
-      <div className="border rounded-md p-4 flex flex-col gap-y-4">
-        <header className="border-b border-dashed pb-4">
-          <p>
-            <strong className={`font-bold text-3xl ${status === "started" ? "text-yellow-300" : status === "done" && "text-green-500"}`}>
-              {
-                status === "todo" ? "Yapılacaklar"
-                  : status === "started" ? "Yapılıyor"
-                    : status === "done" ? "Tamamlandı"
-                      : "Todo"
-              }
-            </strong>
-          </p>
-        </header>
 
-        {
-          status === "todo" &&
-          <form onSubmit={addTodoHandler}>
-            <div className="flex rounded-md overflow-hidden">
+    // const mongoClient = new MongoClient('mongodb+srv://alperksy3434:ftfMdHVLGL9qk9XU@alperdatabase.xr7hjdn.mongodb.net/todo?retryWrites=true&w=majority');
+    // const collection = mongoClient.db().collection('todo');
+    // const data = await collection.find().toArray();
+    // const addData = async () => {
+    //     const result = await collection.insertOne({ name: "deneme", todoContent: "test4" })
+    //     console.log(result)
+    // }
 
-              <input
-                type="text"
-                className="flex-1 text-gray-800 px-4"
-                placeholder="Listeye ekle"
-                onChange={e => setTodoText(e.target.value)}
-                value={todoText}
-              />
-              <button className="flex items-center justify-center py-1.5 w-12 h-8 bg-green-500">
-                <BsPlus
-                  size={28}
-                />
-              </button>
+    return (
+        <>
+            <div className="border rounded-md p-4 flex flex-col gap-y-4">
+                {/* <button onClick={addData}>add</button>  */}
 
+                <header className="border-b border-dashed pb-4">
+                    <p>
+                        <strong className={`font-bold text-3xl ${status === "started" ? "text-yellow-300" : status === "done" && "text-green-500"}`}>
+                            {
+                                status === "todo" ? "Yapılacaklar"
+                                    : status === "started" ? "Yapılıyor"
+                                        : status === "done" ? "Tamamlandı"
+                                            : "Todo"
+                            }
+                        </strong>
+                    </p>
+                </header>
+
+                {
+                    status === "todo" &&
+                    <form onSubmit={addTodoHandler}>
+                        <div className="flex rounded-md overflow-hidden">
+
+                            <input
+                                type="text"
+                                className="flex-1 text-gray-800 px-4"
+                                placeholder="Listeye ekle"
+                                onChange={e => setTodoText(e.target.value)}
+                                value={todoText}
+                            />
+                            <button className="flex items-center justify-center py-1.5 w-12 h-8 bg-green-500">
+                                <BsPlus
+                                    size={28}
+                                />
+                            </button>
+
+                        </div>
+                    </form>
+                }
+
+                <ul className="flex flex-col gap-y-4">
+                    {
+                        todoData.length > 0 &&
+                        todoData.map((data, index) => (
+                            data.status === status && (
+                                <li
+                                    key={index}
+                                    className="flex items-center justify-between"
+                                >
+                                    <p>
+                                        {data.content}
+                                    </p>
+                                    <div className="flex gap-2 text-lg">
+
+                                        <button
+                                            onClick={() => todoStatusHandler(data.id, data.status)}
+                                            className={`border w-8 h-8 grid place-items-center ${status === "todo" ? "border-yellow-300 text-yellow-300" : status === "started" ? "border-green-500 text-green-500" : "hidden"}`}
+                                        >
+                                            {
+                                                data.status !== "started" ?
+                                                    <AiOutlineRight />
+                                                    :
+                                                    <BsCheck
+                                                    />
+                                            }
+                                        </button>
+
+                                        <button
+                                            className={`border w-8 h-8 grid place-items-center border-red-500 text-red-500`}
+
+                                            onClick={() => removeTodo(data.id)}
+                                        >
+                                            <IoIosClose />
+                                        </button>
+                                    </div>
+                                </li>
+                            )
+                        ))
+
+                    }
+                </ul>
             </div>
-          </form>
-        }
-
-        <ul className="flex flex-col gap-y-4">
-          {
-            todoData.length > 0 &&
-            todoData.map((data, index) => (
-              data.status === status && (
-                <li
-                  key={index}
-                  className="flex items-center justify-between"
-                >
-                  <p>
-                    {data.content}
-                  </p>
-                  <div className="flex gap-2 text-lg">
-
-                    <button
-                      onClick={() => todoStatusHandler(data.id, data.status)}
-                      className={`border w-8 h-8 grid place-items-center ${status === "todo" ? "border-yellow-300 text-yellow-300" : status === "started" ? "border-green-500 text-green-500" : "hidden"}`}
-                    >
-                      {
-                        data.status !== "started" ?
-                          <AiOutlineRight />
-                          :
-                          <BsCheck
-                          />
-                      }
-                    </button>
-
-                    <button
-                      className={`border w-8 h-8 grid place-items-center border-red-500 text-red-500`}
-
-                      onClick={() => removeTodo(data.id)}
-                    >
-                      <IoIosClose />
-                    </button>
-                  </div>
-                </li>
-              )
-            ))
-            
-          }
-        </ul>
-      </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default TodoList
